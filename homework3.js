94cceab4d4f4dbf55abd7c6eef3f0c4046eeaa03
LianPetrosyan <99825637+LianPetrosyan@users.noreply.github.com>
2022-03-13 22:50:36 +0400

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -48,7 +48,7 @@
  function isPrime(num){
      if (num<=1) return false;
      debugger
-     for(let i = 2; i * i <= num; i++) {
+     for(let i = 2; sqrt = Math.sqrt(num) ; i <= sqrt; i++) {
          if(num % i === 0) return false;
      }
      arr.push(num);
